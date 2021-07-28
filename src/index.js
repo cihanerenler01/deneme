97c819811cd0f798c7a8996bcaf1330a05424f5b
CihanErenler <87655563+cihanerenler01@users.noreply.github.com>
2021-07-28 19:47:23 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const notes = [
+  { id: 1, note: "HTML is easy" },
+  { id: 2, note: "Browser can execute only Javascript" },
+  {
+    id: 3,
+    note: "GET and POST are the most important methods of HTTP protocol",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
