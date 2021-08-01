d466ea8c1fb2f43552ab55b42abd0c2e77452a6f
Cihan Erenler <cihanerenler@outlook.com>
2021-08-01 17:47:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((res) => setNotes(res.data));
+  });
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      note: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        Show {showAll ? "important" : "all"} notes
+      </button>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          type="text"
+          value={newNote}
+          onChange={(e) => setNewNote(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const notes = [
-  { id: 1, note: "HTML is easy" },
-  { id: 2, note: "Browser can execute only Javascript" },
-  {
-    id: 3,
-    note: "GET and POST are the most important methods of HTTP protocol",
-  },
-];
-
-ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
